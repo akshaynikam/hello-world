4d647b05e4afb5a1b118246a627509a660ea1e97
Akshay Nikam <aknikam@cisco.com>
2020-05-09 15:46:17 +0530

    adjust spacing

--- python/assignmets/ex15f.py
+++ python/assignmets/ex15f.py
@@ -7,7 +7,7 @@
 #* * * *
 #
 
-def pattern_d(n):
+def pattern_f(n):
 	p = ""
 	for i in range(n):
 		for j in range(n - i):
@@ -20,7 +20,7 @@ def pattern_d(n):
 
 def main():
 	n = input("Enter n : ")
-	pattern_d(n)
+	pattern_f(n)
 
 if __name__ == "__main__":
 	main()
